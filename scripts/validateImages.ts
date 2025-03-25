951558d0925eb05138440c35359a8acf03ef81d8
codingwithmanny <manny@berachain.com>
2025-03-26 02:45:41 +0300

    feat: 🎸 Added back checks for both images and metadata images

--- scripts/validateImages.ts
+++ scripts/validateImages.ts
@@ -186,5 +186,5 @@ const validateMetadataImages = () => {
 
 // Initialize
 // ================================================================
-// validateAssetsImages();
+validateAssetsImages();
 validateMetadataImages();
